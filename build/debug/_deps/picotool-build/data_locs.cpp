277d51cf88541ea7168f69c306af084db48247ab
Scott Lanham <cranky.blonde.coder@gmail.com>
2025-06-01 23:17:18 +1000

    Fixing up lack of git ignore.

--- build/debug/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/stodas/devel/repo/pico-hdj100/build/debug/_deps/picotool/"
-};
--- src/pico_hdj100.cpp
+++ src/pico_hdj100.cpp
@@ -4,8 +4,29 @@
 #include "hardware/regs/intctrl.h"
 #include "pico/time.h"
 
-/** The GPIO pin used for . */
-#define _GPIO 1
+/** The GPIO pin used for shutter (venturi) unipolar stepper - coil 1 - section 1. */
+#define SHUTTER_COIL_1_1_GPIO 2
+
+/** The GPIO pin used for shutter (venturi) unipolar stepper - coil 1 - section 2. */
+#define SHUTTER_COIL_1_2_GPIO 3
+
+/** The GPIO pin used for shutter (venturi) unipolar stepper - coil 2 - section 1. */
+#define SHUTTER_COIL_2_1_GPIO 4
+
+/** The GPIO pin used for shutter (venturi) unipolar stepper - coil 2 - section 2. */
+#define SHUTTER_COIL_2_2_GPIO 5
+
+/** State of shutter coil 1 - section 1. Energised is true. */
+bool shutter_coil_1_1;
+
+/** State of shutter coil 1 - section 2. Energised is true. */
+bool shutter_coil_1_2;
+
+/** State of shutter coil 2 - section 1. Energised is true. */
+bool shutter_coil_2_1;
+
+/** State of shutter coil 2 - section 2. Energised is true. */
+bool shutter_coil_2_2;
 
 /**
  * Program for Pi Pico that does Toyota Landcruiser HDJ100 specific things.
@@ -21,13 +42,43 @@ int main()
 	// Must happen for serial stdout to work.
 	stdio_init_all();
 
-	// Push GPIO pin 23 (SMPS Mode) high to disable power saving, which causes noise in the ADC.
-	gpio_init(23);
-	gpio_set_dir(23, GPIO_OUT);
-	gpio_put(23, true);
+	// Setup shutter stepper motor state reading.
 
-	// Main processing loop. Used for user interaction.
+	gpio_init(SHUTTER_COIL_1_1_GPIO);
+	gpio_set_dir(SHUTTER_COIL_1_1_GPIO, GPIO_IN);
+	shutter_coil_1_1 = gpio_get(SHUTTER_COIL_1_1_GPIO);
+
+	gpio_init(SHUTTER_COIL_1_2_GPIO);
+	gpio_set_dir(SHUTTER_COIL_1_2_GPIO, GPIO_IN);
+	shutter_coil_1_2 = gpio_get(SHUTTER_COIL_1_2_GPIO);
+
+	gpio_init(SHUTTER_COIL_2_1_GPIO);
+	gpio_set_dir(SHUTTER_COIL_2_1_GPIO, GPIO_IN);
+	shutter_coil_2_1 = gpio_get(SHUTTER_COIL_2_1_GPIO);
+
+	gpio_init(SHUTTER_COIL_2_2_GPIO);
+	gpio_set_dir(SHUTTER_COIL_2_2_GPIO, GPIO_IN);
+	shutter_coil_2_2 = gpio_get(SHUTTER_COIL_2_2_GPIO);
+
+	// Main processing loop.
 	while(1)
 	{
+		bool prev_shutter_coil_1_1 = shutter_coil_1_1;
+		bool prev_shutter_coil_1_2 = shutter_coil_1_2;
+		bool prev_shutter_coil_2_1 = shutter_coil_2_1;
+		bool prev_shutter_coil_2_2 = shutter_coil_2_2;
+
+		shutter_coil_1_1 = gpio_get(SHUTTER_COIL_1_1_GPIO);
+		shutter_coil_1_2 = gpio_get(SHUTTER_COIL_1_2_GPIO);
+		shutter_coil_2_1 = gpio_get(SHUTTER_COIL_2_1_GPIO);
+		shutter_coil_2_2 = gpio_get(SHUTTER_COIL_2_2_GPIO);
+
+		if(prev_shutter_coil_1_1 != shutter_coil_1_1 ||
+			prev_shutter_coil_1_2 != shutter_coil_1_2 ||
+			prev_shutter_coil_2_1 != shutter_coil_2_1 ||
+			prev_shutter_coil_2_2 != shutter_coil_2_2)
+		{
+			printf("%u - %u - %u - %u", shutter_coil_1_1, shutter_coil_1_2, shutter_coil_2_1, shutter_coil_2_2);
+		}
 	}
 }
